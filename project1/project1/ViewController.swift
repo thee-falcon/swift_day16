dc2d688bdb1bd290a849337796b44f56d453ec35
thee-falcon <omar.makran.10@gmail.com>
2024-03-03 03:07:41 +0100

    Designing our interface

--- project1/project1/ViewController.swift
+++ project1/project1/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 // means I want to create a new screen of data called ViewController, based on UIViewController.
-class ViewController: UIViewController {
+// UITableViewController adds the ability to show rows of data that can be scrolled and selected.
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
-        var pictures = [String]()
-        
         // This super call means “tell Apple’s UIViewController to run its own code before I run mine.
         super.viewDidLoad()
         
@@ -38,7 +39,18 @@ class ViewController: UIViewController {
         }
         print(pictures)
     }
-
-
+    // Showing lots of rows.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  pictures.count
+    }
+    // Dequeuing cells.
+    // The method is called cellForRowAt, and will be called when you need to provide a row.
+    // IndexPath this is a data type that contains both a section number and a row number.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // to display information, it gives the text label of the cell the same text as a picture in our array.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        // set the label text to be the name of the correct picture from our pictures array.
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
-
